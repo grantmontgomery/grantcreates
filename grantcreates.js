da93a0bf7990cc310284ae421465d275434ae91d
Grant <gmontgomery1994@gmail.com>
2019-10-01 17:06:30 -0900

    fixed click and touch functions. light works on everything now as well. last step is fixing the contact.

--- grantcreates.js
+++ grantcreates.js
@@ -1,25 +1,97 @@
-
-
-// ====================KeyboardFunction=====================
+// =================KeyboardFunction===================
 
 window.addEventListener('keydown', function(e){
     let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    if(!audio) return;
+    let light = document.querySelector(`img[data-key="${e.keyCode}"]`)
+    light.style.display = 'block';
+    if(!audio){ return;}
     audio.currentTime = 0;
     audio.play();
 });
 
+window.addEventListener('keyup', function(e){
+    let light = document.querySelector(`img[data-key="${e.keyCode}"]`)
+    light.style.display = 'none';
+});
+
+
 // ====================MouseFunction=====================
 
+// ---------------------MouseFunction controller------------------------
 
-
-window.addEventListener('click', function(e){
+window.addEventListener('mousedown', function(e){
+    let light = e.srcElement.childNodes[3]
     let audio = document.getElementById(e.srcElement.classList[1])
-    if(!audio) return;
-    audio.currentTime = 0;
-    audio.play();
+    if (light.style.display === ""){
+        light.style.display = "block"
+        if(!audio){ return;}
+        audio.currentTime = 0;
+        audio.play();
+    }
+    window.addEventListener('mouseup', function(e){
+        if (light.style.display === "block"){
+            light.style.display = "";
+        }
+    })
 })
 
+// ----------------------MouseFunction instructions--------------------------
 
+window.addEventListener('mouseover', function(e){
+    let singlekeys = document.getElementsByClassName('single-keys')
+    if (e.path[0].attributes[0].textContent === "instructions"){
+        for(let i = 0; i < singlekeys.length; i++){
+            singlekeys[i].style.display = "block";
+        }
+    }
+    else{
+        return;
+    }    
+})
 
+window.addEventListener('mouseout', function(e){
+    let singlekeys = document.getElementsByClassName('single-keys')
+    if (e.path[0].attributes[0].textContent === "instructions"){
+        for(let i = 0; i < singlekeys.length; i++){
+            singlekeys[i].style.display = "";
+        }
+    }
+    else{
+        return;
+    }   
+})
+
+// ---------------------------------MouseFunction contact-------------------------------
+
+window.addEventListener('click', function(e){
+    if(e.path[0].className === 'contact'){
+        document.getElementById('email-phone').style.display = 'block'
+        if (document.getElementById('email-phone').style.display === 'block'){
+            document.getElementById('email-phone').style.display = ''
+        }
+    }
+    else{
+        return;
+    }
+})
+
+// ==========================TouchFunction==============================
+
+// -----------------------------TouchFunction controller-----------------------
+window.addEventListener('touchstart', function(e){
+    let light = e.srcElement.childNodes[3]
+    let audio = document.getElementById(e.srcElement.classList[1])
+    if (light.style.display === ""){
+        light.style.display = "block"
+        if(!audio){ return;}
+        audio.currentTime = 0;
+        audio.play();
+    }
+    window.addEventListener('touchend', function(e){
+        e.preventDefault()
+        if (light.style.display === "block"){
+            light.style.display = "";
+        }
+    })
+})
 
